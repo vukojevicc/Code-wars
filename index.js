4a33ef66cfa92d684e042d1a60e273ae3a3d6141
Aleksandar <aleksandar.vukojevicc@gmail.com>
2021-06-15 20:36:53 +0200

    deleting hash challenge

--- index.js
+++ index.js
@@ -252,27 +252,3 @@ function tickets(peopleInLine){
 }
 
 // tickets([25,50,25,100,25,25,25,100,25,25,50,100]);
-
-// The marketing team is spending way too much time typing in hashtags.
-// Let's help them with our own Hashtag Generator!
-
-// Here's the deal:
-
-// It must start with a hashtag (#).
-// All words must have their first letter capitalized.
-// If the final result is longer than 140 chars it must return false.
-// If the input or the result is an empty string it must return false.
-
-function generateHashtag (str) {
-    for(let i = 0; i < str.length; i++){
-        if(str[i] == ' '){
-            str = str.replace(str[i], '');
-            if(str[i] != ' ' && str[i] != undefined){
-                str = str.replace(str[i], str[i].toUpperCase());
-            }
-        }
-    }
-    console.log(str);
-}
-
-generateHashtag(" Hello there thanks for trying my Kata");
